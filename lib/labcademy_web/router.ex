b1dd38b309903d7b8a2ec006830514e6aed8f876
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2018-03-19 16:31:26 +0100

    With forms

---
+++ lib/labcademy_web/router.ex
@@ -0,0 +1,28 @@
+defmodule LabcademyWeb.Router do
+  use LabcademyWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", LabcademyWeb do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+    get "/form", FormController, :index
+    post "/form", FormController, :create
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", LabcademyWeb do
+  #   pipe_through :api
+  # end
+end
